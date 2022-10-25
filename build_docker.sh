06a3c496d48b25e3fc03921c3da87fefcefafcf4
tpospichal-interset <tomas.pospichal@microfocus.com>
2022-10-25 12:05:04 -0400

    Add interset changes for 2.7.0

---
+++ build_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copyright OpenSearch Contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+VERSION=2.7.0
+PRODUCT=opensearch
+DOCKERFILE=dockerfiles/opensearch.microfocus.dockerfile
+ARCH=x64
+
+pushd docker/release
+./build-image-single-arch.sh -v ${VERSION} -p ${PRODUCT} -f ${DOCKERFILE} -a ${ARCH}
+popd
--- docker/release/build-image-single-arch.sh
+++ docker/release/build-image-single-arch.sh
@@ -7,6 +7,15 @@
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
+# Copyright 2022-2023 OpenText or one of its affiliates.
+# Licensed under Apache-2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#                   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
 # This script is to automate the docker image creation process of OpenSearch and OpenSearch-Dashboards
 
 set -e
@@ -123,6 +132,6 @@ else
 fi
 
 # Docker build
-docker build --build-arg VERSION=$VERSION --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` --build-arg NOTES=$NOTES -f $DOCKERFILE $DIR -t opensearchproject/$PRODUCT:$VERSION
-docker tag opensearchproject/$PRODUCT:$VERSION opensearchproject/$PRODUCT:latest
+docker build --build-arg VERSION=$VERSION --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` --build-arg NOTES=$NOTES -f $DOCKERFILE $DIR -t $PRODUCT:$VERSION
+docker tag $PRODUCT:$VERSION $PRODUCT:latest
 
